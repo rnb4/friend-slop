fc634b623792bf89775b736228c3beeda26665b5
poignantbanana <github.flatly275@passmail.net>
2026-08-15 02:18:37 -0400

    fixedish voip

--- src/player.gd
+++ src/player.gd
@@ -40,11 +40,9 @@ func _ready() -> void:
 		camera.make_current()
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		Steam.startVoiceRecording()
-		print("[voice] authority=%s rate=%d recording started" % [name, _sample_rate])
 	else:
 		voice_player.play()
 		_voice_playback = voice_player.get_stream_playback()
-		print("[voice] playback ready for %s, playback=%s" % [name, _voice_playback])
 
 
 
@@ -75,11 +73,15 @@ func _physics_process(delta: float) -> void:
 		network_position = global_position
 		network_rotation = Vector3(camera.rotation.x, rotation.y, 0.0)
 		_update_interact_subject()
-		_capture_voice()
 	else:
 		global_position = global_position.lerp(network_position, delta * INTERP_SPEED)
 		rotation.y = lerp_angle(rotation.y, network_rotation.y, delta * INTERP_SPEED)
 		camera.rotation.x = lerp_angle(camera.rotation.x, network_rotation.x, delta * INTERP_SPEED)
+
+
+func _process(_delta: float) -> void:
+	if is_multiplayer_authority():
+		_capture_voice()
 
 
 func _update_interact_subject() -> void:
@@ -99,10 +101,10 @@ func _update_interact_subject() -> void:
 func _move(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var dir := (transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
 	if dir:
@@ -111,32 +113,27 @@ func _move(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 		velocity.z = move_toward(velocity.z, 0.0, SPEED)
-
+	
 	move_and_slide()
 
 
 func _capture_voice() -> void:
 	var available: Dictionary = Steam.getAvailableVoice()
-	if Engine.get_physics_frames() % 60 == 0:
-		print("[voice] getAvailableVoice -> %s" % available)
 	if available.get("result") != Steam.VOICE_RESULT_OK or available.get("size", 0) == 0:
 		return
 	var voice: Dictionary = Steam.getVoice()
 	if voice.get("result") == Steam.VOICE_RESULT_OK and voice.get("size", 0) > 0:
-		print("[voice] TX %d bytes" % voice["buffer"].size())
 		_receive_voice.rpc(voice["buffer"])
 
 
 @rpc("authority", "call_remote", "unreliable_ordered")
 func _receive_voice(buffer: PackedByteArray) -> void:
 	var decompressed: Dictionary = Steam.decompressVoice(buffer, _sample_rate)
-	print("[voice] RX %d bytes from %s -> decompress keys=%s result=%s" % [buffer.size(), name, decompressed.keys(), decompressed.get("result", "?")])
 	if decompressed.get("result") != Steam.VOICE_RESULT_OK:
 		return
 	var pcm: PackedByteArray = decompressed["uncompressed"]
 	@warning_ignore("integer_division") var frames := pcm.size() / 2
 	var free := _voice_playback.get_frames_available()
-	print("[voice] PLAY pcm=%d frames=%d free=%d playing=%s" % [pcm.size(), frames, free, voice_player.playing])
 	for i in mini(frames, free):
 		var s := pcm.decode_s16(i * 2) / 32768.0
 		_voice_playback.push_frame(Vector2(s, s))
