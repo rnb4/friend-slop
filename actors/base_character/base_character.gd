f993e512322d618b60495279f5dec926c28bc27c
Lucas Burdell <lucasburdell@gmail.com>
2026-08-15 22:15:26 -0500

    character fix

--- actors/base_character/base_character.gd
+++ actors/base_character/base_character.gd
@@ -3,11 +3,14 @@ extends Node3D
 
 @onready var _animation_tree: BaseCharacterAnimationTree = %AnimationTree
 @onready var _head: MeshInstance3D = %Head
+@onready var _skeleton: Skeleton3D = %GeneralSkeleton
+
 var running: bool = false:
 	set(value):
 		running = value
 		_animation_tree.running = value
 
 func _ready() -> void:
+	_skeleton.show_rest_only = false
 	if is_multiplayer_authority():
 		_head.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_SHADOWS_ONLY
